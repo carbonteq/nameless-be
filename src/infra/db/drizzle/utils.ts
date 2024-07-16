d82218fb59a5dcd39d029815b269a0c008f03680
Arslan <rslnkrmt2552@gmail.com>
2024-07-16 18:29:29 +0500

    better utils for drizzle

--- src/infra/db/drizzle/utils.ts
+++ src/infra/db/drizzle/utils.ts
@@ -1,31 +1,50 @@
-import { BaseEntity, RepositoryResult } from "@carbonteq/hexapp";
+import {
+	BaseEntity,
+	RepositoryResult,
+	assertUnreachable,
+} from "@carbonteq/hexapp";
+
+// https://www.postgresql.org/docs/current/errcodes-appendix.html
+const DrizzleErrors = {
+	UniqueConstraintViolation: "23505",
+	ForeignKeyViolation: "23503",
+} as const;
+type DrizzleErrKey = keyof typeof DrizzleErrors;
+type DrizzleErrCode = (typeof DrizzleErrors)[DrizzleErrKey];
 
 interface DrizzleErr extends Error {
-	code: "23505";
+	code: DrizzleErrCode;
 }
 
 export const isDrizzleErr = (e: unknown): e is DrizzleErr =>
 	/* @ts-ignore*/
 	e instanceof Error && e.code !== undefined;
 
-type DrizzleErrHandlerNames = "UniqueConstaintViolation";
-type DrizzleErrHandler<T extends BaseEntity> = (
-	err: DrizzleErr,
-) => RepositoryResult<T>;
+type DrizzleErrHandler<T> = (err: DrizzleErr) => RepositoryResult<T>;
 
-export const handleDrizzleErr = <
-	Ent extends BaseEntity,
-	T extends Record<DrizzleErrHandlerNames, DrizzleErrHandler<Ent>>,
->(
-	err: unknown,
-	handlers: T,
+const handleOrThrow = <T>(
+	handler: DrizzleErrHandler<T> | undefined,
+	err: DrizzleErr,
 ) => {
+	if (handler === undefined) throw err;
+
+	return handler(err);
+};
+
+export const handleDrizzleErr = <Ent extends BaseEntity>(
+	err: unknown,
+	handlers: Partial<Record<DrizzleErrKey, DrizzleErrHandler<Ent>>>,
+) => {
+	const { ForeignKeyViolation, UniqueConstraintViolation } = handlers;
+
 	if (isDrizzleErr(err)) {
 		switch (err.code) {
-			case "23505":
-				return handlers.UniqueConstaintViolation(err);
+			case DrizzleErrors.UniqueConstraintViolation:
+				return handleOrThrow(UniqueConstraintViolation, err);
+			case DrizzleErrors.ForeignKeyViolation:
+				return handleOrThrow(ForeignKeyViolation, err);
 			default:
-				throw err;
+				assertUnreachable(err.code);
 		}
 	}
 
